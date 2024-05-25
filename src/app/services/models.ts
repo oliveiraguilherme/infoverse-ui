5d4d7008d79a6833b0e0965d34e3e51fb872609b
Guilherme Oliveira <olivetree.silva@gmail.com>
2024-05-25 10:50:28 -0300

    primeiro commit tela de login

---
+++ src/app/services/models.ts
@@ -0,0 +1,5 @@
+/* tslint:disable */
+/* eslint-disable */
+export { AuthenticationRequest } from './models/authentication-request';
+export { AuthenticationResponse } from './models/authentication-response';
+export { RegisterRequest } from './models/register-request';
