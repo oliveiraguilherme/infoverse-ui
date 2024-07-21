3b03dad17113bb59a59779d62f2d9a5169c9eae5
Guilherme Oliveira <olivetree.silva@gmail.com>
2024-07-21 14:50:10 -0300

    feature: add layout with main page and mythos page

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -32,7 +32,7 @@ export class LoginComponent {
     ).subscribe({
       next:(res: AuthenticationResponse) => {
         this.tokenService.token = res.token as string;
-        this.router.navigate(['books'])
+        this.router.navigate(['mythos'])
       },
       error: (err) => {
         console.log(err);
